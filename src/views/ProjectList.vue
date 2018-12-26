6bbeafbb481f2abf1d83df35ff0893f2de372d87
Tony Lin <tonylin1208@gmail.com>
2018-12-26 11:56:49 +0800

    add projectList page

--- src/views/ProjectList.vue
+++ src/views/ProjectList.vue
@@ -1,33 +1,68 @@
 <template>
-  <div class="projectList1">
+  <div class="projectList">
     <h1>專案列表</h1>
+    <div class="operationGroup">
+      <div class="operationGroup-left">
+        <!-- <el-button type="primary" @click="deleteProjects">刪除</el-button> -->
+      </div>
+      <div class="operationGroup-right">
+        <!-- <el-button type="primary" @click="toPath('CreateProject')">
+          <i class="el-icon-plus"></i>
+        </el-button> -->
+      </div>
+    </div>
     <el-table
       :data="projectList"
-      class="projectList-table">
+      class="projectList-table"
+      @selection-change="updateDeleteList">
       <el-table-column
-        prop="id"
-        label="案號"
+        type="selection"
         width="180">
       </el-table-column>
       <el-table-column
+        prop="id"
+        label="案號"
+        width="320">
+        <template slot-scope="scope">
+          <span class="clickable"
+            @click="toPath('EditProject', { projectId: scope.row.id })">
+            {{ scope.row.id }}
+          </span>
+        </template>
+      </el-table-column>
+      <el-table-column
         prop="name"
-        label="名稱">
+        label="名稱"
+        width="460">
       </el-table-column>
     </el-table>
   </div>
 </template>
 
 <script>
+import ToPathMixin from '@/mixins/ToPath'
+
 export default {
   name: 'ProjectList',
-
+  mixins: [ToPathMixin],
   data() {
-    return {}
+    // return {
+    //   deleteList: []
+    // }
   },
   computed: {
-    projectList() {
-      return []
-    }
+  //   userList() {
+  //     return this.$store.getters.projects
+  //   }
+  // },
+  // methods: {
+  //   deleteUsers() {
+  //     if (this.deleteList.length === 0) return
+  //     this.$store.dispatch('deleteProjects', this.deleteList)
+  //   },
+  //   updateDeleteList(value) {
+  //     this.deleteList = value.map(user => user.id)
+  //  }
   }
 }
 </script>
