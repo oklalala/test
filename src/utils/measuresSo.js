0f50646855213884d91afb06ff4e1b57d95329d3
Kai <b10223052@yuntech.org.tw>
2018-12-29 17:39:16 +0800

    use promise in SO api flow

--- src/utils/measuresSo.js
+++ src/utils/measuresSo.js
@@ -1,47 +1,56 @@
+/** @format */
+
 import axios from 'axios'
 
 let wiseConfig = {
-  user: "root",
-  password: "0000",
-  wiseIP: ""
+  user: 'root',
+  password: '0000',
+  wiseIP: ''
 }
-let steps = [
-  initStatus,
-  turnOnPowerDo, turnSoDoOn, readVo,
-  turnSoDoOff, turnSoDoOn, readElt,
-  turnOffPowerDo,
-  computeAndShowData
-];
-// methods , url_path , data
+
+// methods  url_path               data
+// patch    /do_value/slot_0       {"DOVal":[{"Ch":0,"Val":0},{"Ch":1,"Val":0}]}
+// patch    /do_value/slot_0       {"DOVal":[{"Ch":1,"Val":1}]}
+// patch    /do_value/slot_0       {"DOVal":[{"Ch":0,"Val":1}]}:
+// get      /ai_value/slot_0/ch_0  ""
+// patch    /do_value/slot_0       {"DOVal":[{"Ch":0,"Val":0}]}:
+// patch    /do_value/slot_0       {"DOVal":[{"Ch":0,"Val":1}]}:
+// get      /ai_value/slot_0/ch_0  ""
+// patch    /do_value/slot_0       {"DOVal":[{"Ch":1,"Val":0}]}:
 
 export default function(wiseIP, formData) {
-  wiseConfig.wiseIP = "http://" + wiseIP
-  requestMeasuresSo("get", wiseConfig.wiseIP).then(res => {
-    console.log(res.data)
-  })
-  formData.push("fuck")
+  wiseConfig.wiseIP = 'http://' + wiseIP
+  requestMeasuresSo('Patch', '/do_value/slot_0', {DOVal:[{"Ch":0,"Val":0},{"Ch":1,"Val":0}]})
+    .then(() => requestMeasuresSo('Patch', '/do_value/slot_0', {DOVal:[{"Ch":1,"Val":1}]}))
+    .then(() => requestMeasuresSo('Patch', '/do_value/slot_0', {DOVal:[{"Ch":0,"Val":1}]}))
+    .then(() => requestMeasuresSo('Get', '/ai_value/slot_0/ch_0', ''))
+    .then(res => {
+      console.log(res.data)
+      return requestMeasuresSo('Patch', '/do_value/slot_0', {DOVal:[{"Ch":0,"Val":0}]})
+    })
+    .then(()=>requestMeasuresSo('Patch', '/do_value/slot_0', {DOVal:[{"Ch":0,"Val":1}]}))
+    .then(() => requestMeasuresSo('Get', '/ai_value/slot_0/ch_0', ''))
+    .then(res=>{
+      console.log(res.data)
+      return requestMeasuresSo('Patch', '/do_value/slot_0', {DOVal:[{"Ch":1,"Val":0}]})
+    })
+  formData.push('fuck')
 }
 
-function requestMeasuresSo(method, url) {
-  let headers = {
-    Accept: 'application/json',
-    Authorization: makeBasicAuth(wiseConfig.user, wiseConfig.password)
-  }
-  //
-  // if (isAuth) {
-  //   headers['x-access-token'] = store.getters.token
-  // }
-
-  let configs = {
-    baseURL: url,
-    headers
-  }
-
-  return axios.create(configs)[method]("/ai_value/slot_0/ch_0")
+function requestMeasuresSo(method, path, data) {
+  return axios({
+    method: method,
+    url: wiseConfig.wiseIP + path,
+    data: JSON.stringify(data),
+    headers: {
+      Accept: 'application/json',
+      Authorization: makeBasicAuth(wiseConfig.user, wiseConfig.password)
+    }
+  })
 }
 
 function makeBasicAuth(user, password) {
-  var tok = user + ':' + password;
-  var hash = window.btoa(tok);
-  return "Basic " + hash;
+  var tok = user + ':' + password
+  var hash = window.btoa(tok)
+  return 'Basic ' + hash
 }
