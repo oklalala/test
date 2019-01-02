efb57a413eb6c7c53d39346b6ddaf592591add84
Tony Lin <tonylin1208@gmail.com>
2019-01-02 14:48:44 +0800

    fix the ProjectList

--- src/views/ProjectList.vue
+++ src/views/ProjectList.vue
@@ -1,25 +1,31 @@
 <template>
   <div class="projectList">
-    <h1>專案列表</h1>
-    <div class="operationGroup">
-      <div class="operationGroup-left">
-        <el-button type="primary" @click="deleteProjects">刪除</el-button>
-      </div>
-      <div class="operationGroup-right">
-        <el-button type="primary" @click="toPath('ProjectCreate')">
-          <i class="el-icon-plus"></i>
-        </el-button>
-      </div>
-    </div>
+    <el-row :gutter="20" type="flex" justify="space-between">
+      <el-col :span="6">
+      <h1>專案列表</h1>
+      </el-col>
+      <el-col :span="6">
+        <span>專案狀態</span>
+        <el-select placeholder="请选择">
+          <el-option>end</el-option>
+          <el-option>in-progress</el-option>
+        </el-select>
+        <!-- <el-form-item label="專案狀態">
+          <el-select
+            placeholder="1,4"
+            style="width: 100%">
+            <el-option>end</el-option>
+            <el-option>in-progress</el-option>
+          </el-select>
+        </el-form-item> -->
+      </el-col>
+    </el-row>
+    
     <el-table
       :data="projectList"
-      class="projectList-table"
-      @selection-change="updateDeleteList">
+      class="projectList-table">
       <el-table-column
-        type="selection"
-        width="180">
-      </el-table-column>
-      <el-table-column
+        fixed
         prop="id"
         label="案號"
         width="320">
@@ -33,7 +39,21 @@
       <el-table-column
         prop="name"
         label="名稱"
-        width="460">
+        width="320">
+      </el-table-column>
+      <el-table-column
+        label="監控資料"
+        width="180">
+        <template slot-scope="scope">
+          <el-button @click="toPath('SOItem', { soId: scope.row.id })">監控資料</el-button>
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="傾度管量測"
+        width="180">
+        <template slot-scope="scope">
+          <el-button @click="toPath('SOItem', { soId: scope.row.id })">傾度管資料</el-button>
+        </template>
       </el-table-column>
     </el-table>
   </div>
