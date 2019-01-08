3854dbe56ee778b6ad45fcffdc6f208294e4a2fa
Kai <b10223052@yuntech.org.tw>
2019-01-08 10:43:21 +0800

    add delay function

--- src/utils/measuresSo.js
+++ src/utils/measuresSo.js
@@ -18,38 +18,67 @@ let wiseConfig = {
 // get      /ai_value/slot_0/ch_0  ""
 // patch    /do_value/slot_0       {"DOVal":[{"Ch":1,"Val":0}]}:
 
+let powerChannel = 0
+let switchChannel = 1
+let open = 0
+let close = 1
+
 export default function(wiseIP, formData) {
   wiseConfig.wiseIP = 'http://' + wiseIP
+  // delay()
   initializationSO()
-    .then(() =>switchSO(1,1))
-    .then(() =>switchSO(0,1))
-    .then(() =>getSORawData())
-    .then(res => {
-      console.log(res.data);
-      switchSO(0,0)
-    })
-    .then(() =>switchSO(0,1))
-    .then(() => getSORawData())
-    .then(res => {
-      console.log(res.data);
-      switchSO(1,0)
-    })
+  .then(()=>switchSO(powerChannel,open))
+  .then(()=>switchSO(switchChannel,open))
+  .then(() =>getSORawData())
+  .then(res=>{
+    console.log(res.data)
+  })
+  .then(()=>switchSO(switchChannel,close))
+  .then(()=>switchSO(switchChannel,open))
+  .then(() =>getSORawData())
+  .then(res=>{
+    console.log(res.data)
+  })
+  .then(() =>switchSO(switchChannel,close))
+  .then(() =>switchSO(switchChannel,open))
+  .then(() =>getSORawData())
+  .then(res => {
+    console.log(res.data);
+  })
+  // .then(() =>switchSO(0,1))
+  // .then(() => getSORawData())
+  // .then(res => {
+  //   console.log(res.data);
+  //   // switchSO(1,0)
+  // })
   formData.push('fuck')
 }
 
-function initializationSO(){
+// 加個 delay function 不讓他太快切換
+
+function initializationSO() {
   return requestMeasuresSO('Patch', '/do_value/slot_0', {
-    DOVal: [{ Ch: 0, Val: 0 }, { Ch: 1, Val: 0 }]
+    DOVal: [{
+      Ch: powerChannel,
+      Val: close
+    }, {
+      Ch: switchChannel,
+      Val: close
+    }]
   })
 }
 
-function switchSO(channel,value){
+function switchSO(channel, value) {
+  delay()
   return requestMeasuresSO('Patch', '/do_value/slot_0', {
-    DOVal: [{ Ch: channel, Val: value }]
+    DOVal: [{
+      Ch: channel,
+      Val: value
+    }]
   })
 }
 
-function getSORawData(){
+function getSORawData() {
   return requestMeasuresSO('Get', '/ai_value/slot_0/ch_0', '')
 }
 
@@ -69,4 +98,11 @@ function makeBasicAuth(user, password) {
   var tok = user + ':' + password
   var hash = window.btoa(tok)
   return 'Basic ' + hash
+}
+
+function delay() {
+  for (let i = 0; i < 1000; i++) {
+    console.log(i)
+    // console.clear()
+  }
 }
