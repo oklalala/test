a2e4ca68080123b1806eff41b8f68fa3258feb98
Tony Lin <tonylin1208@gmail.com>
2019-02-04 13:16:01 +0800

    add RWD in UserInfo

--- src/views/UserInfo.vue
+++ src/views/UserInfo.vue
@@ -1,68 +1,72 @@
 <template>
   <div class="user-info">
-    <h1>帳號資料</h1>
-    <el-form>
-      <section>
-        <h2>基本資料</h2>
-        <p>姓名：{{ me.name }}</p>
-        <p>角色：{{ me.roleName}}</p>
-        <p v-if="me.company">公司：{{ me.company.name}}</p>
-        <p v-if="me.soItem">傾度管 編號：{{ me.soItem.number}}</p>
-      </section>
-      <section>
-        <h2>聯絡方式</h2>
-          <el-form-item label="電話">
-            <el-input
-              :value="me.phone"
-              @change="value => this.updateMe({ phone: value })"
-              :disabled="!isShow()"
-              placeholder="請輸入電話">
-            </el-input>
-          </el-form-item>
-          <el-form-item label="email">
-            <el-input
-              :value="me.email"
-              @change="value => this.updateMe({ email: value })"
-              :disabled="!isShow()"
-              placeholder="請輸入 email">
-            </el-input>
-          </el-form-item>
-      </section>
-      <section>
-        <h2>密碼設定</h2>
-        <el-form-item label="密碼">
-          <el-input
-            :value="me.password"
-            :type='password'
-            @change="value => this.updateMe({ password: value })"
-            :disabled="!isShow()"
-            placeholder="請輸入密碼">
-          </el-input>
-        </el-form-item>
-        <el-checkbox @change='showPassword()'>顯示密碼</el-checkbox>
-      </section>
-      <section>
-        <h2>參與專案</h2>
-        <el-table
-          :data="me.projects"
-          style="width: 100%">
-          <el-table-column
-            prop="id"
-            label="案號"
-            width="180">
-          </el-table-column>
-          <el-table-column
-            prop="name"
-            label="名稱">
-          </el-table-column>
-        </el-table>
-      </section>
-      <div class="button-container">
-        <el-button type="primary" @click="submit">
-          確認
-        </el-button>
-      </div>
-    </el-form>
+    <el-row type='flex' justify='center'>
+      <el-col :md='8' :span='18'>
+        <h1>帳號資料</h1>
+        <el-form>
+          <section>
+            <h3>基本資料</h3>
+            <p>姓名：{{ me.name }}</p>
+            <p>角色：{{ me.roleName}}</p>
+            <p v-if="me.company">公司：{{ me.company.name}}</p>
+            <p v-if="me.soItem">傾度管 編號：{{ me.soItem.number}}</p>
+          </section>
+          <section>
+            <h3>聯絡方式</h3>
+              <el-form-item label="電話">
+                <el-input
+                  :value="me.phone"
+                  @change="value => this.updateMe({ phone: value })"
+                  :disabled="!isShow()"
+                  placeholder="請輸入電話">
+                </el-input>
+              </el-form-item>
+              <el-form-item label="email">
+                <el-input
+                  :value="me.email"
+                  @change="value => this.updateMe({ email: value })"
+                  :disabled="!isShow()"
+                  placeholder="請輸入 email">
+                </el-input>
+              </el-form-item>
+          </section>
+          <section>
+            <h3>密碼設定</h3>
+            <el-form-item label="密碼">
+              <el-checkbox @change='showPassword()' style='float:right;'>顯示密碼</el-checkbox>
+              <el-input
+                :value="me.password"
+                :type='password'
+                @change="value => this.updateMe({ password: value })"
+                :disabled="!isShow()"
+                placeholder="請輸入密碼">
+              </el-input>
+            </el-form-item>
+          </section>
+          <section>
+            <h3>參與專案</h3>
+            <el-table
+              :data="me.projects"
+              style="width: 100%">
+              <el-table-column
+                prop="id"
+                label="案號"
+                width="180">
+              </el-table-column>
+              <el-table-column
+                prop="name"
+                label="名稱">
+              </el-table-column>
+            </el-table>
+          </section>
+          <div class="button-container">
+            <el-button type="primary" @click="submit">
+              確認
+            </el-button>
+          </div>
+        </el-form>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
