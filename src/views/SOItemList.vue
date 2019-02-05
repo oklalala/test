111710cd725ec29ab5f9fe277cb46d4211ed8800
Tony Lin <tonylin1208@gmail.com>
2019-02-05 11:06:31 +0800

    add RWD of SOItemList

--- src/views/SOItemList.vue
+++ src/views/SOItemList.vue
@@ -1,46 +1,47 @@
 <template>
-  <div class="soList">
-    <h1>傾度管列表</h1>
-    <div class="operationGroup">
-      <div class="operationGroup-left">
-        <el-button type="primary" @click="deleteSOItems" v-if="!!deleteList.length">刪除</el-button>
+  <el-row class="soList" type='flex' justify='center'>
+    <el-col :span='22' :sm='18' :md='14'>
+      <h1>傾度管列表</h1>
+      <div class="operationGroup">
+        <div class="operationGroup-left">
+          <el-button type="primary" @click="deleteSOItems" v-if="!!deleteList.length">刪除</el-button>
+        </div>
+        <div class="operationGroup-right">
+          <el-button type="primary" @click="toPath('SOItemCreate')">
+            <i class="el-icon-plus"></i>
+          </el-button>
+        </div>
       </div>
-      <div class="operationGroup-right">
-        <el-button type="primary" @click="toPath('SOItemCreate')">
-          <i class="el-icon-plus"></i>
-        </el-button>
-      </div>
-    </div>
-    <el-table
-      :data="soItems"
-      class="soItems-table"
-      @selection-change="updateDeleteList">
-      <el-table-column
-        type="selection"
-        width="40">
-      </el-table-column>
-      <el-table-column
-        label="編號"
-        width="180">
-        <template slot-scope="scope">
-          <span class="clickable"
-            @click="toPath('SOItemEdit', { soId: scope.row.id })">
-            {{ scope.row.number }}
-          </span>
-        </template>
-      </el-table-column>
-      <el-table-column
-        prop="soModelName"
-        label="廠牌 型號"
-        width="120">
-      </el-table-column>
-      <el-table-column
-        prop="OPT"
-        label="OPT"
-        width="120">
-      </el-table-column>
-    </el-table>
-  </div>
+      <el-table
+        :data="soItems"
+        class="soItems-table"
+        @selection-change="updateDeleteList">
+        <el-table-column
+          type="selection"
+          width="40">
+        </el-table-column>
+        <el-table-column
+          label="編號"
+          width="100">
+          <template slot-scope="scope">
+            <span class="clickable"
+              @click="toPath('SOItemEdit', { soId: scope.row.id })">
+              {{ scope.row.number }}
+            </span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="soModelName"
+          label="廠牌 型號"
+          width="120">
+        </el-table-column>
+        <el-table-column
+          prop="OPT"
+          label="OPT">
+        </el-table-column>
+      </el-table>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
