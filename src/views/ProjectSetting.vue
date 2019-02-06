a51f48be3cf323b5ced194b3c83ed3f3b5e322f3
Tony Lin <tonylin1208@gmail.com>
2019-02-06 19:53:22 +0800

    add RWD of ProjectSetting

--- src/views/ProjectSetting.vue
+++ src/views/ProjectSetting.vue
@@ -1,52 +1,53 @@
 <template>
-  <div class="projectSetting">
-    <h1>專案設定</h1>
-    <el-row :gutter="20" type="flex" justify="space-between">
-      <el-col :span="6">
-        <el-button type="primary" @click="deleteProjects" v-show="!!deleteList.length">刪除</el-button>
-      </el-col>
-      <el-col :offset="10">
-        <el-button type="primary" @click="toPath('ProjectCreate')">
-          <i class="el-icon-plus"></i>
-        </el-button>
-      </el-col>
-    </el-row>
-    <el-table
-      :data="projectList"
-      class="projectList-table"
-      @selection-change="updateDeleteList">
-      <el-table-column
-        type="selection"
-        width="30">
-      </el-table-column>
-      <el-table-column
-        prop="id"
-        label="案號"
-        width="200">
-        <template slot-scope="scope">
-          <span class="clickable"
-            @click="toPath('ProjectEdit', { projectId: scope.row.id })">
-            {{ scope.row.id }}
-          </span>
-        </template>
-      </el-table-column>
-      <el-table-column
-        prop="name"
-        label="名稱"
-        width="200">
-      </el-table-column>
-      <el-table-column
-        prop="status"
-        label="專案狀態"
-        width="100"
-        :filters="[{ text: '結案', value: 'end' }, { text: '執行', value: 'in-progress' }]"
-        :filter-method="statusFilter">
-        <template slot-scope="scope">
-          <el-tag :type="scope.row.status === 'end' ? 'success' : 'warning'" disable-transitions>{{scope.row.status}}</el-tag>
-        </template>
-      </el-table-column>
-    </el-table>
-  </div>
+  <el-row class="projectSetting" type='flex' justify='center'>
+    <el-col :span='22' :sm='18' :md='14'>
+      <h1>專案設定</h1>
+      <div class="operationGroup">
+        <div class="operationGroup-left">
+          <el-button type="primary" @click="deleteProjects" v-show="!!deleteList.length">刪除</el-button>
+        </div>
+        <div class="operationGroup-right">
+          <el-button type="primary" @click="toPath('ProjectCreate')">
+            <i class="el-icon-plus"></i>
+          </el-button>
+        </div>
+      </div>
+      <el-table
+        :data="projectList"
+        class="projectList-table"
+        @selection-change="updateDeleteList">
+        <el-table-column
+          type="selection"
+          width="30">
+        </el-table-column>
+        <el-table-column
+          prop="id"
+          label="案號"
+          width="200">
+          <template slot-scope="scope">
+            <span class="clickable"
+              @click="toPath('ProjectEdit', { projectId: scope.row.id })">
+              {{ scope.row.id }}
+            </span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="名稱"
+          width="200">
+        </el-table-column>
+        <el-table-column
+          prop="status"
+          label="專案狀態"
+          :filters="[{ text: '結案', value: 'end' }, { text: '執行', value: 'in-progress' }]"
+          :filter-method="statusFilter">
+          <template slot-scope="scope">
+            <el-tag :type="scope.row.status === 'end' ? 'success' : 'warning'" disable-transitions>{{scope.row.status}}</el-tag>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
