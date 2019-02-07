384c727dbdd380588f216e10ba29c6e87dda3091
Tony Lin <tonylin1208@gmail.com>
2019-02-07 23:58:55 +0800

    add RWD of ProjectPhaseList

--- src/views/ProjectPhaseList.vue
+++ src/views/ProjectPhaseList.vue
@@ -1,58 +1,59 @@
 <template>
-  <div class="soList">
-    <h1>專案執行階段</h1>
-    <div class="operationGroup">
-      <div class="operationGroup-left">
-        <el-button type="primary" @click="deleteProjectPhase">刪除</el-button>
+  <el-row class="projectSetting" type='flex' justify='center'>
+    <el-col :span='22' :sm='18' :md='14'>
+      <h1>專案執行階段</h1>
+      <div class="operationGroup">
+        <div class="operationGroup-left">
+          <el-button type="primary" @click="deleteProjectPhase">刪除</el-button>
+        </div>
+        <div class="operationGroup-right">
+          <el-form
+            label-position="left"
+            label-width="90px"
+            :model="newProjectPhase">
+            <el-form-item label="新增專案：">
+              <el-input v-model="newProjectPhase.name"
+                placeholder="第二次開挖">
+              </el-input>
+            </el-form-item>
+            <el-button type="primary" @click="createProjectPhase" v-if="!!newProjectPhase.name">
+              <i class="el-icon-plus"></i>
+            </el-button>
+          </el-form>
+        </div>
       </div>
-      <div class="operationGroup-right">
-        <el-form
-          label-position="left"
-          label-width="90px"
-          :model="newProjectPhase">
-          <el-form-item label="新增專案：">
-            <el-input v-model="newProjectPhase.name"
-              placeholder="第二次開挖">
+      <el-table
+        :data="projectPhaseList"
+        class="projectPhaseList-table"
+        @selection-change="updateDeleteList">
+        <el-table-column
+          type="selection"
+          width="50">
+        </el-table-column>
+        <el-table-column
+          prop="id"
+          label="序號"
+          width="170">
+          <template slot-scope="scope">
+            <span class="clickable"
+              @click="toPath('ProjectPhaseEdit', { soId: scope.row.id })">
+              {{ scope.row.id }}
+            </span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="專案階段">
+          <template slot-scope="scope">
+            <el-input 
+              @keyup.native="editProjectPhase(scope.row.id,scope.row.name)"
+              v-model="scope.row.name">
             </el-input>
-          </el-form-item>
-          <el-button type="primary" @click="createProjectPhase" v-if="!!newProjectPhase.name">
-            <i class="el-icon-plus"></i>
-          </el-button>
-        </el-form>
-      </div>
-    </div>
-    <el-table
-      :data="projectPhaseList"
-      class="projectPhaseList-table"
-      @selection-change="updateDeleteList">
-      <el-table-column
-        type="selection"
-        width="50">
-      </el-table-column>
-      <el-table-column
-        prop="id"
-        label="序號"
-        width="180">
-        <template slot-scope="scope">
-          <span class="clickable"
-            @click="toPath('ProjectPhaseEdit', { soId: scope.row.id })">
-            {{ scope.row.id }}
-          </span>
-        </template>
-      </el-table-column>
-      <el-table-column
-        prop="name"
-        label="專案階段"
-        width="200">
-        <template slot-scope="scope">
-          <el-input 
-            @keyup.native="editProjectPhase(scope.row.id,scope.row.name)"
-            v-model="scope.row.name">
-          </el-input>
-        </template>
-      </el-table-column>
-    </el-table>
-  </div>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
