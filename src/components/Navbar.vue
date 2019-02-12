725e70d7656f8f6865d251e43701e3bdea144075
Tony Lin <tonylin1208@gmail.com>
2019-02-13 01:13:49 +0800

    change navbar style

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,51 +4,23 @@
       <h1 class="layout-navbar-title" style="color: #fff">大地監控</h1>
     </el-header>
     <section id="nav">
-      <ul class="nav__main">
-        <li @click="toPath('UserInfo')">帳號資料</li>
-        <li @click="toPath('ProjectList')">專案列表</li>
+      <ul class="nav__items">
+        <li class="nav__item" tabindex='0' @click="toPath('UserInfo')">帳號資料</li>
+        <li class="nav__item" tabindex='0' @click="toPath('ProjectList')">專案列表</li>
       </ul>
-      <ul class="nav__sub">
-        <li @click="toPath('VGList')" v-if="isShow('vg:CRUD')">軸力計設定</li>
-        <li @click="toPath('SOItemList')" v-if="isShow('soItem:CRUD')">傾度管設定</li>
-        <li @click="toPath('UserList')" v-if="isShow('account:CRUD')">帳號設定</li>
+      <ul class="nav__items">
+        <li class="nav__item" tabindex='0' @click="toPath('VGList')" v-if="isShow('vg:CRUD')">軸力計設定</li>
+        <li class="nav__item" tabindex='0' @click="toPath('SOItemList')" v-if="isShow('soItem:CRUD')">傾度管設定</li>
+        <li class="nav__item" tabindex='0' @click="toPath('UserList')" v-if="isShow('account:CRUD')">帳號設定</li>
         <el-submenu>
           <template slot="title" id='popupLevel2'>專案設定</template>
-          <li @click="toPath('ProjectSetting')">專案資料</li>
-          <li @click="toPath('ProjectPhaseList')">執行階段</li>
+          <li class="nav__item" tabindex='0' @click="toPath('ProjectSetting')">專案資料</li>
+          <li class="nav__item" tabindex='0' @click="toPath('ProjectPhaseList')">執行階段</li>
         </el-submenu>
-        <li @click="toPath('PermissionSetup')" v-if="isShow('permission:CRUD')">權限設定</li>
-        <li class='submenu' @click="logout">登出</li>
+        <li class="nav__item" tabindex='0' @click="toPath('PermissionSetup')" v-if="isShow('permission:CRUD')">權限設定</li>
+        <li class="nav__item" tabindex='0' @click="logout">登出</li>
       </ul>
     </section>
-    <el-main>
-      <template v-if="isLogined" class="layout-navbar-menu">
-        <template class="layout-navbar-menu-forAdmin">
-          <el-menu 
-            class="el-menu-vertical-demo"
-            background-color="#545c64"
-            text-color="#fff"
-            mode="horizontal"
-            active-text-color="#ffd04b">
-            <el-menu-item index="1" @click="toPath('UserInfo')">帳號資料</el-menu-item>
-            <el-menu-item index="2" @click="toPath('ProjectList')">專案列表</el-menu-item>
-            <el-menu-item index="4" class='submenu' @click="logout">登出</el-menu-item>
-            <el-submenu index="3" class='submenu'>
-              <template slot="title" id='popupLevel1'>設定</template>
-              <el-menu-item index="3-1" @click="toPath('VGList')" v-if="isShow('vg:CRUD')">軸力計設定</el-menu-item>
-              <el-menu-item index="3-2" @click="toPath('SOItemList')" v-if="isShow('soItem:CRUD')">傾度管設定</el-menu-item>
-              <el-menu-item index="3-3" @click="toPath('UserList')" v-if="isShow('account:CRUD')">帳號設定</el-menu-item>
-              <el-submenu index="3-4">
-                <template slot="title" id='popupLevel2'>專案設定</template>
-                <el-menu-item index="3-4-1" @click="toPath('ProjectSetting')">專案資料</el-menu-item>
-                <el-menu-item index="3-4-2" @click="toPath('ProjectPhaseList')">執行階段</el-menu-item>
-              </el-submenu>
-              <el-menu-item index="3-5" @click="toPath('PermissionSetup')" v-if="isShow('permission:CRUD')">權限設定</el-menu-item>
-            </el-submenu>
-          </el-menu>
-        </template>
-      </template>
-    </el-main>
   </el-container>
 </template>
 
@@ -80,37 +52,42 @@ export default {
 </script>
 
 <style>
+#nav {
+  display: flex;
+  justify-content: space-between;
+  background:#545c64;
+}
+.nav__items {
+  padding: 0;
+  margin: 0;
+}
+.nav__item {
+  display: inline-block;
+  padding: 0 15px;
+  font-size: 14px;
+  line-height: 60px;
+  color: white;
+  cursor: pointer;
+  border-bottom: 3px solid transparent;
+}
+.nav__item:hover {
+  background: #434a50;
+}
+
+.nav__item:focus {
+  outline: none;
+  color: #ffd04b;
+  border-bottom: 3px solid #ffd04b;
+}
+
+
 .layout-navbar-title {
   font-size: 30px;
   background: #545c64;
   padding: 15px;
   margin: 0;
 }
-
-.el-header,
-.el-main {
+.el-header {
   padding: 0;
-}
-
-.el-menu-item.submenu,
-.el-submenu.submenu {
-  float: right;
-}
-
-/* justify the position of popup in setting dropdown list */
-.el-menu--popup.el-menu--popup-bottom-start {
-  left: 50px;
-  min-width: 100px;
-}
-
-/* justify the position of sub dropdown in projectSetting */
-li.is-opened > div.el-menu--horizontal {
-  /* element UI default setting, it's hard to change */
-  left: -85px !important;
-  top: 108px !important;
-}
-.el-menu--horizontal > ul,
-.el-submenu .el-menu-item {
-  min-width: 70px;
 }
 </style>
