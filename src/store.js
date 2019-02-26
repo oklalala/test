7ff9c8ba53a0e83c902fc77849b48f1226c06b70
Tony Lin <tonylin1208@gmail.com>
2019-02-27 02:23:45 +0800

    refactor getUser in vuex

--- src/store.js
+++ src/store.js
@@ -137,16 +137,23 @@ const companies = {
 
 const users = {
   state: {
-    data: []
+    data: [],
+    currentOne: {}
   },
   mutations: {
     setUsers(state, users) {
       state.data = users
+    },
+    setUser(state, user) {
+      state.currentOne = user
     }
   },
   getters: {
     users(state) {
       return state.data
+    },
+    currentUser(state) {
+      return state.currentOne
     },
     OPTs(state) {
       return state.data.filter(user => user.roleName == 'OPT')
@@ -156,8 +163,13 @@ const users = {
     }
   },
   actions: {
-    getUser(context, userId) {
-      return sendAPI('get', `/user/${userId}`, true)
+    getUser({ commit }, userId) {
+      return sendAPI('get', `/user/${userId}`, true).then(res => {
+        commit('setUser', res.data.data)
+      })
+    },
+    updateCurrentUser({ commit }, payload) {
+      commit('setUser', payload)
     },
     updateUser(context, { userId, payload }) {
       return sendAPI('put', `/user/${userId}`, true, payload)
