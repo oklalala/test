ad5e208c2fd056a69de3d6828bf7d9783876299c
Tony <apple@AppledeMacBook-Pro.local>
2019-03-09 12:46:24 +0800

    add validate of phone and email

--- src/views/UserInfo.vue
+++ src/views/UserInfo.vue
@@ -2,7 +2,9 @@
   <el-row class="user-info" type='flex' justify='center'>
     <el-col  :xs='18' :sm='12' :md='12'>
       <h1>帳號資料</h1>
-      <el-form>
+      <el-form
+        :model='me'
+        :rules='rules'>
         <section>
           <h3>基本資料</h3>
           <p>姓名：{{ me.name }}</p>
@@ -10,24 +12,24 @@
           <p v-if="me.company">公司：{{ me.company.name}}</p>
           <p v-if="me.soItem">傾度管 編號：{{ me.soItem.number}}</p>
         </section>
+        <h3>聯絡方式</h3>
         <section>
-          <h3>聯絡方式</h3>
-            <el-form-item label="電話">
-              <el-input
-                :value="me.phone"
-                @change="value => this.updateMe({ phone: value })"
-                :disabled="!isShow()"
-                placeholder="請輸入電話">
-              </el-input>
-            </el-form-item>
-            <el-form-item label="email">
-              <el-input
-                :value="me.email"
-                @change="value => this.updateMe({ email: value })"
-                :disabled="!isShow()"
-                placeholder="請輸入 email">
-              </el-input>
-            </el-form-item>
+          <el-form-item label="電話" prop="phone">
+            <el-input
+              :value="me.phone"
+              @change="value => this.updateMe({ phone: value })"
+              :disabled="!isShow()"
+              placeholder="請輸入電話">
+            </el-input>
+          </el-form-item>
+          <el-form-item label="email" prop='email'>
+            <el-input
+              :value="me.email"
+              @change="value => this.updateMe({ email: value })"
+              :disabled="!isShow()"
+              placeholder="請輸入 email">
+            </el-input>
+          </el-form-item>
         </section>
         <section>
           <h3>密碼設定</h3>
@@ -61,13 +63,22 @@ export default {
   mixins: [ToPathMixin],
   data() {
     return {
-      password: 'password'
+      password: 'password',
+      rules:{
+        phone: [ 
+            { required: true, pattern: /[0][9]\d{8}/, message: '請輸入電話號碼 : 09xxxxxxxx', trigger: 'blur' }
+        ],
+        email: [
+          { required: true, message: '請輸入 email', trigger: 'blur' },
+          { type: 'email', message: '請確認格式 example@chuen.com.tw', trigger: ['blur', 'change'] }
+        ],
+      }
     }
   },
   computed: {
     me() {
       return this.$store.getters.me
-    }
+    },
   },
   methods: {
     updateMe(payload) {
