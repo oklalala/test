0755465c110ac5a65e78f004d50f1c78e3e55c65
chris <dwatow@gmail.com>
2019-04-06 11:31:53 +0800

    fix API of fetch so model

--- src/store/soModels/actions.js
+++ src/store/soModels/actions.js
@@ -4,7 +4,7 @@ import API from '@/utils/API'
 
 export default {
   async getSOModels({ commit }) {
-    const res = await API.GET('so-models')
+    const res = await API.GET('/so-models')
     return commit('setSOModels', res.data)
   }
 }
